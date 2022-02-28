72cee72b2141e6eecf774d8d6aa6fdc13ae85f25
inmanturbo <inman.turbo@gmail.com>
2022-02-28 08:35:49 -0500

    work in progress

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Onecentlin\Adminer\ServiceProvider::class,
 
         /*
          * Application Service Providers...
